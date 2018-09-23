479133c893d621c2572dfd01448c8b8a777b6091
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2018-09-23 14:32:42 +0200

    Logging now compacts multiple consecutive movement steps

--- events/eventlogger.go
+++ events/eventlogger.go
@@ -6,13 +6,28 @@ var ALL_EVENT_NAMES = []string{INPUT_EVENT_NAME, INTERACTION_EVENT_NAME, MOVEMEN
 
 func InitEventLogging(outfunc func(a ...interface{}) (i int, e error)) {
 
+	var lastMessage ActionEvent
+
 	for _, eventName := range ALL_EVENT_NAMES {
 		engo.Mailbox.Listen(eventName, func(msg engo.Message) {
 			eventMsg, ok := msg.(ActionEvent)
 			if !ok {
 				panic("Trying to log an event that isn't an action event, this shouldn't happen!" + msg.Type())
 			}
+
+			if lastMoveMsg, ok := lastMessage.(MovementEvent); ok {
+				if currMoveMsg, currentIsMoveMsg := eventMsg.(MovementEvent); currentIsMoveMsg && currMoveMsg.Action == MOVEMENT_EVENT_ACTION_STEP {
+					lastMessage = eventMsg
+					// don't print it
+					return
+				} else {
+					outfunc("[ Multiple MOVEMENT_EVENTs ]")
+					outfunc(lastMoveMsg.Type(), lastMoveMsg.AsLogMessage())
+				}
+			}
+
 			outfunc(eventMsg.Type(), eventMsg.AsLogMessage())
+			lastMessage = eventMsg
 		})
 	}
 
