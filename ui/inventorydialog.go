25c18b5ae73d383e556b380118244713e4995a87
MrTrustworthy <tinywritingmakesmailhardtoread@gmail.com>
2018-10-19 21:38:25 +0200

    minor clean up

--- ui/inventorydialog.go
+++ ui/inventorydialog.go
@@ -15,14 +15,13 @@ func NewInventoryDialog(inventory *entities.Inventory) *Dialog {
 	for item, amount := range *inventory {
 
 		btnPosition := engo.AABB{
-			Min: engo.Point{X: 120, Y: float32(120+offset)},
-			Max: engo.Point{X: 380, Y: float32(220+offset)},
+			Min: engo.Point{X: 120, Y: float32(120 + offset)},
+			Max: engo.Point{X: 380, Y: float32(220 + offset)},
 		}
-		btn := NewButton(btnPosition, item.Name + ": " + strconv.Itoa(amount), func(btn *Button) { fmt.Println("Clicked on " + item.Name) })
+		btn := NewButton(btnPosition, item.Name+": "+strconv.Itoa(amount), func(btn *Button) { fmt.Println("Clicked on " + item.Name) })
 		d.AddElement(btn)
 		offset += 20
 	}
-
 
 	return d
 }
